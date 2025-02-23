ba44798c09b6e9c09ee9aafe867224e77216577b
Luotio <juha@luotio.net>
2025-02-23 11:06:52 +0200

    * Added OpenFolder dialog

--- GitVibe.Gui/Program.cs
+++ GitVibe.Gui/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Avalonia;
+using Avalonia.ReactiveUI;
 using System;
-
 namespace GitVibe;
 
 sealed class Program
@@ -17,5 +17,6 @@ sealed class Program
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .UseReactiveUI();
 }
--- GitVibe.Gui/ViewModels/MainWindowViewModel.cs
+++ GitVibe.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,28 +1,39 @@
-﻿
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
 using GitVibe.Model;
 
+using ReactiveUI ;
+using GitVibe.Views;
 namespace GitVibe.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public ObservableCollection<GitLogEntry> GitLog { get; } = new()
-    {
-        new GitLogEntry("Initial commit", "John Doe"),
-        new GitLogEntry("Add feature", "Jane Doe"),
-    };
+  public ObservableCollection<GitLogEntry> GitLog { get; } = new()
+  {
+    new GitLogEntry("Initial commit", "John Doe"),
+    new GitLogEntry("Add feature", "Jane Doe"),
+  };
 
-    public string Greeting { get; } = "Welcome to Avalonia!";
+  public string Greeting { get; } = "Welcome to Avalonia!";
 
-    public ObservableCollection<FolderItem> FolderTree { get; } = new()
-    {
-        new FolderItem("Root")
-        {
-            Children = new ObservableCollection<FolderItem>
-            {
-                new FolderItem("Folder 1"),
-                new FolderItem("Folder 2")
-            }
-        }
-    };
+  public ObservableCollection<FolderItem> FolderTree { get; } = new()
+  {
+      new FolderItem("Root")
+      {
+          Children = new ObservableCollection<FolderItem>
+          {
+              new FolderItem("Folder 1"),
+              new FolderItem("Folder 2")
+          }
+      }
+  };
+
+  public ICommand OpenFolderCommand { get; set; }
+
+  public MainWindowViewModel()
+  {
+
+  }
+
 }

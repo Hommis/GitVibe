1f3a1accf1ea76a7c7d9815a2485f8249eb515be
Juha Luotio <juha@luotio.net>
2025-02-23 18:22:52 +0200

    * Added drawing

--- GitVibe.Gui/Controls/GitCommitGraph.cs
+++ GitVibe.Gui/Controls/GitCommitGraph.cs
@@ -20,7 +20,10 @@ namespace GitVibe.Gui.Controls;
         public sealed override void Render(DrawingContext context)
         {
           var renderSize = Bounds.Size;
-          context.FillRectangle(Brushes.Red , new Rect(renderSize));
+          var x = 10 ;
+          var pen = new Pen(Brushes.RoyalBlue, 3);
+          context.DrawLine(pen , new Point(x,0), new Point(x, renderSize.Height));
+          context.DrawEllipse(Brushes.White, pen, new Point( x, renderSize.Height / 2 ), 5,5 );
           base.Render(context);
         }
     }
